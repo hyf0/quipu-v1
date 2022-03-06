5d9517f250039bbe6d30cc0574f9da36ecb6b6fb
Yunfei He <i.heyunfei@gmail.com>
2022-03-06 20:16:14 +0800

    style

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import { FC } from 'react'
 export const Header: FC = () => {
   return (
     <Link href="/" passHref={true}>
-      <h1 className="text-4xl border-b p-2">Blog</h1>
+      <h1 className="text-4xl border-b p-2 cursor-pointer">Blog</h1>
     </Link>
   )
 }
